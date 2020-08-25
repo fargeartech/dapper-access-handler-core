a17b6d1ae120c3b8fa3e9d504876d5f1c15c5e4c
faris <faris@twincontinent.my>
2020-08-25 08:27:12 +0800

    add pagination class and calculate offset function

---
+++ src/DapperHelper/Pagination/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DapperHelper.Pagination
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
